594a171cfb16b7fe34b622e6e6d0699263e7bf92
Kaushik-Shetti-Kilowott <kaushik.shetti@kilowott.com>
2022-11-07 15:30:08 +0530

    2

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000;
 
-app.use(cors())
+//app.use(cors())
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json())
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
