47182d92b4202cd2e11ca17d9905a12af9bcef24
Kaushik-Shetti-Kilowott <92842022+Kaushik-Shetti-Kilowott@users.noreply.github.com>
2022-11-07 13:10:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
-
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
   // ... you will write your Prisma Client queries here
 //   const allUsers = await prisma.account.findMany()
