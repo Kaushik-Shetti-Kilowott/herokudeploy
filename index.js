908faae679e1e864bd9ba03823f3d313fc2cabbb
Kaushik-Shetti-Kilowott <kaushik.shetti@kilowott.com>
2022-11-07 16:14:53 +0530

    6

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
